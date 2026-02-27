3e58ea85b00a087a73c871c64d8df3f217d61acc
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-02-27 20:29:23 +0300

    fix(api): changed schema -> model

--- backend/app/crud/task_logic.py
+++ backend/app/crud/task_logic.py
@@ -1,5 +1,3 @@
-from collections.abc import Sequence
-
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -7,26 +5,29 @@ from app.models.task_models import Task
 from app.schemas.task_schemas import TaskBase, TaskCreate, TaskUpdate
 
 
-async def get_tasks(db: AsyncSession, skip: int = 0, limit: int = 100) -> Sequence[TaskBase]:
+async def get_tasks(db: AsyncSession, skip: int = 0, limit: int = 100) -> list[TaskBase]:
     result = await db.scalars(select(Task).where(Task.is_active).offset(skip).limit(limit))
     tasks = result.all()
     return [TaskBase.model_validate(task) for task in tasks]
 
 
 async def get_task(db: AsyncSession, task_id: int) -> TaskBase | None:
-    return await db.get(TaskBase, task_id)
+    task = await db.get(Task, task_id)
+    if task and task.is_active:
+        return TaskBase.model_validate(task)
+    return None
 
 
 async def create_task(db: AsyncSession, task_in: TaskCreate) -> TaskBase:
-    task = TaskBase(**task_in.model_dump())
+    task = Task(**task_in.model_dump())
     db.add(task)
     await db.commit()
     await db.refresh(task)
-    return task
+    return TaskBase.model_validate(task)
 
 
 async def update_task(db: AsyncSession, task_id: int, task_in: TaskUpdate) -> TaskBase | None:
-    task = await db.get(TaskBase, task_id)
+    task = await db.get(Task, task_id)
     if not task:
         return None
     update_data = task_in.model_dump(exclude_unset=True)
@@ -34,13 +35,13 @@ async def update_task(db: AsyncSession, task_id: int, task_in: TaskUpdate) -> Ta
         setattr(task, field, value)
     await db.commit()
     await db.refresh(task)
-    return task
+    return TaskBase.model_validate(task)
 
 
 async def delete_task(db: AsyncSession, task_id: int) -> bool:
-    task = await db.get(TaskBase, task_id)
+    task = await db.get(Task, task_id)
     if not task:
         return False
-    await db.delete(task)
+    task.is_active = False
     await db.commit()
     return True
