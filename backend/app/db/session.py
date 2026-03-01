f32e664e621a3cdfae286e92c2e9f1cd32bfe193
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-01 18:28:07 +0300

    fix(db): del init db

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -7,10 +7,5 @@ engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-async def init_db() -> None:
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-
 class Base(DeclarativeBase):
     pass
