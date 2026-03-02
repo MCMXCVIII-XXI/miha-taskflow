40f2f26707003ab626d6b742b78bef236e142ca7
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-02 21:18:01 +0300

    feat(schemas): add Group schemas

---
+++ backend/app/schemas/group_schemas.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class UserGroupBase(BaseModel):
+    name: str = Field(..., max_length=50, description="Group name")
+
+
+class UserGroupCreate(UserGroupBase):
+    pass
+
+
+class UserGroup(UserGroupBase):
+    id: int
+    admin_id: int
+    is_active: bool
+    created_at: datetime
+    updated_at: datetime | None
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserGroupMembershipBase(BaseModel):
+    user_id: int
+    group_id: int
+
+
+class UserGroupMembershipCreate(UserGroupMembershipBase):
+    pass
+
+
+class UserGroupMembership(UserGroupMembershipBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime | None
+    is_active: bool
+
+    model_config = ConfigDict(from_attributes=True)
