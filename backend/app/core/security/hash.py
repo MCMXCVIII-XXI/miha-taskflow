3e6e53a0d48ce277d8f6a2268ca935f195325a58
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-02 21:03:12 +0300

    feat(security): add hashed

---
+++ backend/app/core/security/hash.py
@@ -0,0 +1,23 @@
+from argon2 import PasswordHasher, exceptions
+from argon2.low_level import Type
+
+password_hasher = PasswordHasher(
+    time_cost=2,
+    memory_cost=65536,
+    parallelism=4,
+    hash_len=32,
+    salt_len=16,
+    type=Type.ID,
+)
+
+
+def get_password_hash(password: str) -> str:
+    return password_hasher.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    try:
+        password_hasher.verify(plain_password, hashed_password)
+        return True
+    except exceptions.VerifyMismatchError:
+        return False
