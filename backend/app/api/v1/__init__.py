f728c66e65837560eea3a0443d24a93978386122
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-04 21:17:06 +0300

    feat(api): add users router

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 
-from .endpoints import tasks
+from .endpoints import tasks, users
 
 api_router = APIRouter()
 api_router.include_router(tasks.router, prefix="/api/v1/tasks", tags=["tasks"])
+api_router.include_router(users.router, prefix="/api/v1/users", tags=["users"])
+
+
+__all__ = ["api_router"]
