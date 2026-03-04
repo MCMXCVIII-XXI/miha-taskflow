0cd3c2d6adc964bcaaa64835eb767b8c39f20c96
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-04 21:20:03 +0300

    fix(api): fixed logic task

--- backend/app/crud/task_logic.py
+++ backend/app/crud/task_logic.py
@@ -1,53 +1,95 @@
+from collections.abc import Sequence
+
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import Task
-from app.schemas.task_schemas import TaskBase, TaskCreate, TaskUpdate
+from app.schemas.task_schemas import TaskCreate, TaskUpdate
+
+from .crud_result import CrudResultTask
 
 
 async def get_tasks(
     db: AsyncSession, skip: int = 0, limit: int = 100
-) -> list[TaskBase]:
-    result = await db.scalars(
-        select(Task).where(Task.is_active).offset(skip).limit(limit)
+) -> Sequence[Task]:
+    tasks = await db.scalars(
+        select(Task).order_by(Task.id).where(Task.is_active).offset(skip).limit(limit)
     )
-    tasks = result.all()
-    return [TaskBase.model_validate(task) for task in tasks]
+    return tasks.all()
 
 
-async def get_task(db: AsyncSession, task_id: int) -> TaskBase | None:
-    task = await db.get(Task, task_id)
-    if task and task.is_active:
-        return TaskBase.model_validate(task)
-    return None
+async def get_task(task_id: int, db: AsyncSession) -> Task | CrudResultTask:
+    result = await db.scalars(select(Task).where(Task.id == task_id, Task.is_active))
+    task = result.first()
+
+    if not task:
+        return CrudResultTask.NOT_FOUND
+
+    return task
 
 
-async def create_task(db: AsyncSession, task_in: TaskCreate) -> TaskBase:
+async def create_task(task_in: TaskCreate, db: AsyncSession) -> Task | CrudResultTask:
+    result = await db.scalars(
+        select(Task).where(
+            (Task.title == task_in.title),
+            (Task.group_id == task_in.group_id),
+            Task.is_active,
+        )
+    )
+
+    check = result.first()
+
+    if check:
+        return CrudResultTask.TITLE_CONFLICT
+
     task = Task(**task_in.model_dump())
     db.add(task)
     await db.commit()
     await db.refresh(task)
-    return TaskBase.model_validate(task)
+    return task
 
 
 async def update_task(
-    db: AsyncSession, task_id: int, task_in: TaskUpdate
-) -> TaskBase | None:
-    task = await db.get(Task, task_id)
+    task_id: int, task_in: TaskUpdate, db: AsyncSession
+) -> Task | CrudResultTask:
+    result = await db.scalars(select(Task).where(Task.id == task_id, Task.is_active))
+    task = result.first()
+
     if not task:
-        return None
+        return CrudResultTask.NOT_FOUND
+
     update_data = task_in.model_dump(exclude_unset=True)
+
+    # Check if task with this title or group already exists
+    ###########################################################################
+    result = await db.scalars(
+        select(Task).where(
+            (Task.title == update_data["title"]),
+            (Task.group_id == update_data["group_id"]),
+            Task.id != task_id,
+            Task.is_active,
+        )
+    )
+    check = result.first()
+    if check:
+        return CrudResultTask.TITLE_CONFLICT
+    ###########################################################################
+
     for field, value in update_data.items():
         setattr(task, field, value)
+
     await db.commit()
     await db.refresh(task)
-    return TaskBase.model_validate(task)
+    return task
 
 
-async def delete_task(db: AsyncSession, task_id: int) -> bool:
-    task = await db.get(Task, task_id)
+async def delete_task(task_id: int, db: AsyncSession) -> bool | CrudResultTask:
+    result = await db.scalars(select(Task).where(Task.id == task_id, Task.is_active))
+    task = result.first()
+
     if not task:
-        return False
+        return CrudResultTask.NOT_FOUND
+
     task.is_active = False
     await db.commit()
     return True
