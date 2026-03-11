af465a82f4ef6965ec0fb0fc1179f501a3140cb9
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-11 17:26:18 +0300

    fix(api): change for task_logic.py and rename schema/model

--- backend/app/api/v1/endpoints/tasks.py
+++ backend/app/api/v1/endpoints/tasks.py
@@ -1,76 +1,79 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
+from app.core.security.current_role import RoleCurrentUser
 from app.crud.crud_result import CrudResultTask
 from app.crud.task_logic import create_task as create
-from app.crud.task_logic import (
-    delete_task as delete,
-)
-from app.crud.task_logic import (
-    get_task,
-    get_tasks,
-)
-from app.crud.task_logic import (
-    update_task as update,
-)
+from app.crud.task_logic import delete_task as delete
+from app.crud.task_logic import get_group_tasks as get_group_tasks_logic
+from app.crud.task_logic import get_task, get_tasks, get_user_tasks
+from app.crud.task_logic import update_task as update
 from app.db import db_helper
-from app.schemas.task_schemas import TaskBase, TaskCreate, TaskUpdate
+from app.models import User as UserModel
+from app.schemas.task_schemas import Task as TaskSchemas
+from app.schemas.task_schemas import TaskCreate, TaskUpdate
 
 router = APIRouter(prefix="", tags=["tasks"])
 
 
-@router.post("/", response_model=TaskBase, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=TaskSchemas, status_code=status.HTTP_201_CREATED)
 async def create_task(
     task_in: TaskCreate, db: AsyncSession = Depends(db_helper.get_session)
-) -> TaskBase:
+) -> TaskSchemas:
     task = await create(task_in, db)
 
     if task == CrudResultTask.TITLE_CONFLICT:
         raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT, detail="Task already exists"
+            status_code=status.HTTP_409_CONFLICT,
+            detail=CrudResultTask.TITLE_CONFLICT.value,
         )
 
-    return TaskBase.model_validate(task)
+    return TaskSchemas.model_validate(task)
 
 
-@router.get("/", response_model=list[TaskBase], status_code=status.HTTP_200_OK)
+@router.get("/", response_model=list[TaskSchemas], status_code=status.HTTP_200_OK)
 async def read_tasks(
-    skip: int = 0, limit: int = 100, db: AsyncSession = Depends(db_helper.get_session)
-) -> list[TaskBase]:
+    skip: int = 0,
+    limit: int = 100,
+    db: AsyncSession = Depends(db_helper.get_session),
+) -> list[TaskSchemas]:
     tasks = await get_tasks(db, skip, limit)
-    return [TaskBase.model_validate(task) for task in tasks]
+    return [TaskSchemas.model_validate(task) for task in tasks]
 
 
-@router.get("/{task_id}", response_model=TaskBase, status_code=status.HTTP_200_OK)
+@router.get("/{task_id}", response_model=TaskSchemas, status_code=status.HTTP_200_OK)
 async def read_task(
-    task_id: int, db: AsyncSession = Depends(db_helper.get_session)
-) -> TaskBase:
+    task_id: int,
+    db: AsyncSession = Depends(db_helper.get_session),
+    current_user: UserModel = Depends(RoleCurrentUser.admin),
+) -> TaskSchemas:
     task = await get_task(task_id, db)
 
     if task == CrudResultTask.NOT_FOUND:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Task not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail=CrudResultTask.NOT_FOUND.value
         )
 
-    return TaskBase.model_validate(task)
+    return TaskSchemas.model_validate(task)
 
 
-@router.put("/{task_id}", response_model=TaskBase, status_code=status.HTTP_200_OK)
+@router.put("/{task_id}", response_model=TaskSchemas, status_code=status.HTTP_200_OK)
 async def update_task_endpoint(
     task_id: int, task_in: TaskUpdate, db: AsyncSession = Depends(db_helper.get_session)
-) -> TaskBase:
+) -> TaskSchemas:
     task = await update(task_id, task_in, db)
 
     if task == CrudResultTask.NOT_FOUND:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Task not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail=CrudResultTask.NOT_FOUND.value
         )
     if task == CrudResultTask.TITLE_CONFLICT:
         raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT, detail="Task already exists"
+            status_code=status.HTTP_409_CONFLICT,
+            detail=CrudResultTask.TITLE_CONFLICT.value,
         )
 
-    return TaskBase.model_validate(task)
+    return TaskSchemas.model_validate(task)
 
 
 @router.delete("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -80,5 +83,34 @@ async def delete_task_endpoint(
     result = await delete(task_id, db)
     if result == CrudResultTask.NOT_FOUND:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Task not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail=CrudResultTask.NOT_FOUND.value
         )
+
+
+@router.get(
+    "/me/tasks", response_model=list[TaskSchemas], status_code=status.HTTP_200_OK
+)
+async def get_me_tasks(
+    skip: int = 0,
+    limit: int = 100,
+    db: AsyncSession = Depends(db_helper.get_session),
+    current_user: UserModel = Depends(RoleCurrentUser.member),
+) -> list[TaskSchemas]:
+    tasks = await get_user_tasks(current_user, db, skip, limit)
+    return [TaskSchemas.model_validate(task) for task in tasks]
+
+
+@router.get(
+    "/group/{group_id}/tasks",
+    response_model=list[TaskSchemas],
+    status_code=status.HTTP_200_OK,
+)
+async def get_group_tasks(
+    group_id: int,
+    skip: int = 0,
+    limit: int = 100,
+    db: AsyncSession = Depends(db_helper.get_session),
+    current_user: UserModel = Depends(RoleCurrentUser.admin_groups),
+) -> list[TaskSchemas]:
+    tasks = await get_group_tasks_logic(group_id, db, skip, limit)
+    return [TaskSchemas.model_validate(task) for task in tasks]
