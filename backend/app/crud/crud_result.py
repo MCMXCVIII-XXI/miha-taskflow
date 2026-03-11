4a39932484958bfb8f190583811e6e79294d8398
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-11 16:58:23 +0300

    feat(crud): add response enums for endpoints

--- backend/app/crud/crud_result.py
+++ backend/app/crud/crud_result.py
@@ -10,3 +10,13 @@ class CrudResultUser(Enum):
     NOT_FOUND = "User not found"
     EMAIL_CONFLICT = "Email already exists"
     USERNAME_CONFLICT = "Username already exists"
+
+
+class CrudResultGroup(Enum):
+    NOT_FOUND = "Group not found"
+    NAME_CONFLICT = "Name already exists"
+
+
+class CrudResultGroupMembership(Enum):
+    NOT_FOUND = "Group membership not found"
+    MEMBER_CONFLICT = "Member already exists"
