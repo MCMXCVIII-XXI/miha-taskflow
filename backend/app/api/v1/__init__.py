266059b135d9e168c3c38cf006d2cf91d74fc7c5
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-11 18:16:07 +0300

    fix(api): del group_memberships

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from .endpoints import groups, groups_memberships, tasks, users
+from .endpoints import groups, tasks, users
 
 api_router = APIRouter()
 api_router.include_router(tasks.router, prefix="/api/v1/tasks", tags=["tasks"])
@@ -8,4 +8,4 @@ api_router.include_router(users.router, prefix="/api/v1/users", tags=["users"])
 api_router.include_router(groups.router, prefix="/api/v1/groups", tags=["groups"])
 
 
-__all__ = ["api_router", "groups_memberships"]
+__all__ = ["api_router"]
