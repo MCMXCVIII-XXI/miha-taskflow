201021d2d171ec3319bddb1ac6c20a2c5d7e0e15
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-11 16:43:14 +0300

    feat(api): add router groups

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from .endpoints import tasks, users
+from .endpoints import groups, groups_memberships, tasks, users
 
 api_router = APIRouter()
 api_router.include_router(tasks.router, prefix="/api/v1/tasks", tags=["tasks"])
 api_router.include_router(users.router, prefix="/api/v1/users", tags=["users"])
+api_router.include_router(groups.router, prefix="/api/v1/groups", tags=["groups"])
 
 
-__all__ = ["api_router"]
+__all__ = ["api_router", "groups_memberships"]
