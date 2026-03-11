a3ed3b179db5a39dc5138d574a9dc178928f4e49
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-11 16:54:21 +0300

    feat(config): edited BaseSettings
    - add TokenSettings
    - changed the schema for the database to correctly define variables from .env

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,13 +2,24 @@ from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings):
-    DATABASE_URL: PostgresDsn
+class TokenSettings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 30
+    model_config = SettingsConfigDict(
+        env_file=".env", env_prefix="TOKEN_", extra="ignore"
+    )
+
+
+class DBSettings(BaseSettings):
+    URL: PostgresDsn
     echo: bool = False
     echo_pool: bool = False
     pool_size: int = 5
     max_overflow: int = 10
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", env_prefix="DB_", extra="ignore")
 
 
-settings = Settings()
+token_settings = TokenSettings()
+db_settings = DBSettings()
