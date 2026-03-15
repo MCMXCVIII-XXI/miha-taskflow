5ee79d1a7eb7f5de2ff15ba967e3d266b701404a
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-16 02:54:01 +0300

    feat(rbac): add
    - class RBAC for init
    - function for permissions user
    - function for check permissions

---
+++ backend/app/core/permission/check_permission.py
@@ -0,0 +1,59 @@
+from fastapi import Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db import db_helper
+from app.models import Permission, Role, RolePermission
+from app.models import UserRole as UserRoleModel
+from app.schemas.user_schemas import User as UserSchema
+
+from ..exceptions import security_exc
+from ..security.auth import get_current_user
+
+
+async def get_user_permissions_db(user_id: int, db: AsyncSession) -> set[str]:
+    """
+    Get user permissions from database
+    """
+    result = await db.scalars(
+        select(Permission.name)  # select all permissions from the database
+        .join(
+            RolePermission, Permission.id == RolePermission.permission_id
+        )  # select all role permissions
+        .join(Role, RolePermission.role_id == Role.id)  # select all roles
+        .join(UserRoleModel, UserRoleModel.role_id == Role.id)  # select all user roles
+        .where(UserRoleModel.user_id == user_id)  # filter by user id
+    )
+    return set(result.all())
+
+
+def require_permissions_db(
+    *required_permissions: str,
+) -> Callable:  # type: ignore
+    """
+    Check if user has required permissions
+
+    Args:
+        required_permissions (list[str]): List of required permissions
+
+        Args:
+            current_user (UserSchema): Current user
+            db (AsyncSession): Database session
+
+    Details:
+        This function checks if the user has the required permissions.
+        If the user does not have the required permissions,
+        it raises a SecurityPermissionDenied exception.
+    """
+
+    async def dependency(
+        current_user: UserSchema = Depends(get_current_user),
+        db: AsyncSession = Depends(db_helper.get_session),
+    ) -> UserSchema:
+        perms = await get_user_permissions_db(current_user.id, db)
+        missing = [p for p in required_permissions if p not in perms]
+        if missing:
+            raise security_exc.SecurityPermissionDenied(details={"Missing": missing})
+        return current_user
+
+    return dependency
---
+++ backend/app/core/permission/init_rbac.py
@@ -0,0 +1,98 @@
+from sqlalchemy import delete, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db import Base, db_helper
+from app.models import Permission, Role, RolePermission, UserRole
+
+from .seed_data import SeedData
+from .setup_role_permissions import SetupRolePermissions
+
+
+class RBAC:
+    """
+    Class for working with RBAC
+
+    Details:
+        This class is responsible for working with RBAC.
+        It includes methods for checking if tables exist,
+        adding data, clearing tables, and binding roles and permissions.
+
+        Init should be done after migrations.
+
+        seed: instance of class SeedData
+        setup: instance of class SetupRolePermissions
+
+    Args:
+        db (AsyncSession): Database session
+        models (tuple[type[Base], ...]): Models for RBAC
+        seed (SeedData): Seed data for RBAC
+        setup (SetupRolePermissions): Setup role permissions
+    """
+
+    def __init__(
+        self,
+        db: AsyncSession,
+        models: tuple[type[Base], ...],
+        seed: SeedData,
+        setup: SetupRolePermissions,
+    ) -> None:
+        self.db = db
+        self.models = models
+        self._seed = seed
+        self._setup = setup
+
+    # Method for checking if tables exist
+    # roles, permissions, role_permissions, user_roles
+    async def __check(self) -> bool:
+        """
+        Check if tables exist
+        """
+        for model in self.models:
+            result = await self.db.scalars(select(model).limit(1))
+            if result.first():
+                return False
+        return True
+
+    # Initial seeding of RBAC
+    # Adding roles and permissions
+    # Creating many to many relationship between roles and permissions
+    # (Table role_permissions)
+    # Creating many to many relationship between users and roles (Table user_roles)
+    async def init(self) -> None:
+        """
+        Initial seeding of RBAC
+        """
+        # Checking tables ##################
+        if await self.__check():
+            # Clear tables and binds ###########
+            await self.__clear()
+            # Adding data ######################
+            await self._seed.seed()
+            # Binds role and permissions #######
+            await self._setup.setup_all()
+            ####################################
+
+    # Clear tables and binds
+    async def __clear(self) -> None:
+        """
+        Clear tables and binds
+        """
+        for model in self.models:
+            await self.db.scalars(delete(model))
+        await self.db.commit()
+
+
+async def init_rbac() -> RBAC:
+    """
+    Get RBAC
+    """
+    # Models for RBAC
+    # First models this binds "many to many"
+    MODELS = (RolePermission, UserRole, Role, Permission)
+
+    # Get RBAC for initial seeding and setup
+    async with db_helper.get_session_ctx() as db:
+        rbac = RBAC(
+            db=db, models=MODELS, seed=SeedData(db), setup=SetupRolePermissions(db)
+        )
+        await rbac.init()
