e1b9fe3acbb7c9ba2a39d8b0057b36324f113175
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-16 03:05:13 +0300

    feat(app): add models RBAC for import

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,15 @@
 from .group_models import UserGroup, UserGroupMembership
+from .role_models import Permission, Role, RolePermission, UserRole
 from .task_models import Task
 from .user_models import User
 
-__all__ = ["Task", "User", "UserGroup", "UserGroupMembership"]
+__all__ = [
+    "Permission",
+    "Role",
+    "RolePermission",
+    "Task",
+    "User",
+    "UserGroup",
+    "UserGroupMembership",
+    "UserRole",
+]
