75e1e55512d18e0b31db2d3ea02e102271677811
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-16 03:04:38 +0300

    feat(app): add new router login

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
-from .endpoints import groups, tasks, users
+from .endpoints import groups, login, tasks, users
 
 api_router = APIRouter()
 api_router.include_router(tasks.router, prefix="/api/v1/tasks", tags=["tasks"])
 api_router.include_router(users.router, prefix="/api/v1/users", tags=["users"])
 api_router.include_router(groups.router, prefix="/api/v1/groups", tags=["groups"])
+api_router.include_router(login.router, prefix="/api/v1/login", tags=["login"])
 
 
 __all__ = ["api_router"]
