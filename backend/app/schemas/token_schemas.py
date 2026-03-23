db44628600fa9abcffbb2ac58d9da90ed83d49af
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-23 03:20:19 +0300

    fix(security): changed schemas Token

--- backend/app/schemas/token_schemas.py
+++ backend/app/schemas/token_schemas.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel, Field
 
 
 class TokenResponse(BaseModel):
-    access_token: str | None = Field(default=None, alias="Access Token")
-    refresh_token: str | None = Field(default=None, alias="Refresh Token")
-    token_type: str = Field(default="bearer", alias="Token Type")
+    access_token: str | None = Field(default=None, description="Access Token")
+    refresh_token: str | None = Field(default=None, description="Refresh Token")
+    token_type: str = Field(default="bearer", description="Token Type")
 
 
 class RefreshTokenRequest(BaseModel):
-    refresh_token: str = Field(alias="Refresh Token")
+    refresh_token: str = Field(description="Refresh Token")
 
 
 class AccessTokenRequest(BaseModel):
-    access_token: str = Field(alias="Access Token")
+    access_token: str = Field(description="Access Token")
