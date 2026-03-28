c506964f2611de3ae4edbb0c0f913bc6883f9224
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-28 06:48:29 +0300

    fix(import): fix __init__.py for include routers

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,12 +1,10 @@
 from fastapi import APIRouter
 
-from .endpoints import groups, login, tasks, users
+from .endpoints import auth_router, groups_router, tasks_router, users_router
 
 api_router = APIRouter()
-api_router.include_router(tasks.router, prefix="/api/v1/tasks", tags=["tasks"])
-api_router.include_router(users.router, prefix="/api/v1/users", tags=["users"])
-api_router.include_router(groups.router, prefix="/api/v1/groups", tags=["groups"])
-api_router.include_router(login.router, prefix="/api/v1/login", tags=["login"])
 
-
-__all__ = ["api_router"]
+api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
+api_router.include_router(groups_router, prefix="/groups", tags=["groups"])
+api_router.include_router(tasks_router, prefix="/tasks", tags=["tasks"])
+api_router.include_router(users_router, prefix="/users", tags=["users"])
