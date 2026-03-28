12a52e715bef7ea38188d474f6583d07fffb64f4
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-28 06:58:38 +0300

    feat(api): rewrote auth endpoints

---
+++ backend/app/api/v1/endpoints/auth.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter, Depends, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from app.db import db_helper
+from app.schemas import RefreshTokenRequest, TokenResponse, UserCreate
+from app.service import AuthenticationService, get_authentication_service
+
+router = APIRouter()
+
+
+@router.post("/token", response_model=TokenResponse, status_code=status.HTTP_200_OK)
+async def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: AsyncSession = Depends(db_helper.get_session),
+    svc: AuthenticationService = Depends(get_authentication_service),
+) -> TokenResponse:
+    return await svc.login(form_data)
+
+
+@router.post("/", response_model=TokenResponse, status_code=status.HTTP_200_OK)
+async def register(
+    user_in: UserCreate,
+    db: AsyncSession = Depends(db_helper.get_session),
+    svc: AuthenticationService = Depends(get_authentication_service),
+) -> TokenResponse:
+    return await svc.register(user_in)
+
+
+@router.post(
+    "/access-token", response_model=TokenResponse, status_code=status.HTTP_200_OK
+)
+async def access_token(
+    body: RefreshTokenRequest,
+    db: AsyncSession = Depends(db_helper.get_session),
+    svc: AuthenticationService = Depends(get_authentication_service),
+) -> TokenResponse:
+    return await svc.access_token(body)
+
+
+@router.post(
+    "/refresh-token",
+    response_model=TokenResponse,
+    status_code=status.HTTP_200_OK,
+)
+async def refresh_token(
+    body: RefreshTokenRequest,
+    db: AsyncSession = Depends(db_helper.get_session),
+    svc: AuthenticationService = Depends(get_authentication_service),
+) -> TokenResponse:
+    return await svc.refresh_token(body)
