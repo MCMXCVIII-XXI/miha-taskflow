a2b8791f82ae6181b573a9386b7b4e18db61db32
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-28 08:18:50 +0300

    fix(models): add type_checking and add description in UserGroup and  created_at, UniqueConstraint in UserGroupMembership

--- backend/app/models/group.py
+++ backend/app/models/group.py
@@ -1,15 +1,21 @@
 from datetime import datetime
+from typing import TYPE_CHECKING
 
-from sqlalchemy import Boolean, DateTime, ForeignKey, String, func
+from sqlalchemy import Boolean, DateTime, ForeignKey, String, UniqueConstraint, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db import Base
 from app.db.mixins import IdPkMixin
 
+if TYPE_CHECKING:
+    from .task import Task
+    from .user import User
+
 
 class UserGroup(Base, IdPkMixin):
     name: Mapped[str] = mapped_column(String(50), unique=True, index=True)
-    admin_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    admin_id: Mapped[int] = mapped_column(ForeignKey("users.id"), index=True)
+    description: Mapped[str | None] = mapped_column(String(255), nullable=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now()
     )
@@ -17,6 +23,7 @@ class UserGroup(Base, IdPkMixin):
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
     is_active: Mapped[bool] = mapped_column(Boolean, default=True, index=True)
+
     tasks: Mapped[list["Task"]] = relationship(
         "Task", back_populates="group", cascade="all, delete-orphan"
     )
@@ -29,5 +36,12 @@ class UserGroupMembership(Base, IdPkMixin):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     group_id: Mapped[int] = mapped_column(ForeignKey("user_groups.id"))
     is_active: Mapped[bool] = mapped_column(Boolean, default=True, index=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
     user: Mapped["User"] = relationship("User", back_populates="group_memberships")
     group: Mapped["UserGroup"] = relationship("UserGroup", back_populates="users")
+
+    __table_args__ = (
+        UniqueConstraint("user_id", "group_id", name="uq_user_group_membership"),
+    )
