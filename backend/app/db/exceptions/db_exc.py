a403f7a495b9c924b04c8f2c971ef72a9f517383
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-28 08:10:24 +0300

    feat(exc): create new custom exceptions for db

---
+++ backend/app/db/exceptions/db_exc.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+from fastapi import status
+
+
+class BaseDBError(Exception):
+    """Base database error."""
+
+    def __init__(
+        self,
+        code: int,
+        message: str,
+        headers: dict[str, str] | None = None,
+        details: dict[str, Any] | None = None,
+    ) -> None:
+        self.code = code
+        self.message = message
+        self.headers = headers
+        self.details = details
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class DBConnectionError(BaseDBError):
+    """Database connection error."""
+
+    def __init__(
+        self,
+        message: str,
+        headers: dict[str, str] | None = None,
+        details: dict[str, Any] | None = None,
+    ) -> None:
+        super().__init__(
+            code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            message=message,
+            headers=headers,
+            details=details,
+        )
