6e3ded8dbfedca44ce6897ff77dd76b767d85fa5
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-03-29 04:52:40 +0300

    feat(import): add PERMISSIONS

--- backend/app/core/permission/__init__.py
+++ backend/app/core/permission/__init__.py
@@ -1,4 +1,5 @@
 from .check_permission import require_permissions_db
 from .init_rbac import init_rbac
+from .permissions import PERMISSIONS
 
-__all__ = ["init_rbac", "require_permissions_db"]
+__all__ = ["PERMISSIONS", "init_rbac", "require_permissions_db"]
