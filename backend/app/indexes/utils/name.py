5e1523fdd149f8882f4706fa9d8787239a31ba8d
Ilya Gimaletdinov <mcmxcviii@internet.ru>
2026-05-06 04:49:01 +0300

    refactor(documents): rename indexes module to documents

--- backend/app/indexes/utils/name.py
+++
@@ -1,7 +0,0 @@
-from .settings_index import load_index_mappings
-
-
-def get_index_name(index_key: str) -> str:
-    """Get index name by key."""
-    prefix = load_index_mappings().get(index_key, {}).get("prefix")
-    return f"{prefix}_{index_key}" if prefix else index_key
